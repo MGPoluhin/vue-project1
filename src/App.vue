5fb224282d7aac19e7fa4e5ac13cf59f54b58343
MGPoluhin <99122209+MGPoluhin@users.noreply.github.com>
2022-06-06 12:20:30 +0500

    add postlist and sort

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <div class="app__btns">
+      <my-button
+          @click="showDialog"
+      >
+        Создать пост
+      </my-button>
+      <my-select
+        v-model="selectedSort"
+        :options="sortOptions"
+      />
+    </div>
+
+    <my-dialog v-model:show="dialogVisible" >
+      <post-form
+          @create="CreatePost"
+      />
+    </my-dialog>
+
+    <post-list
+        :posts="posts"
+        @remove="removePost"
+        v-if="isPostLoading"
+    />
+    <div v-else>Идет загрузка...</div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import MyDialog from "@/components/UI/MyDialog";
+import MyButton from "@/components/UI/MyButton";
+import axios from 'axios';
+import MySelect from "@/components/UI/MySelect";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    MySelect,
+    MyButton,
+    MyDialog,
+    PostList,PostForm
+  },
+  name: "App.vue",
+  data(){
+    return{
+      posts: [],
+      dialogVisible: false,
+      isPostLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По описанию'},
+      ]
+    }
+  },
+  methods:{
+    CreatePost(post){
+      this.posts.push(post)
+      this.dialogVisible = false
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostLoading = false;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        this.posts = response.data;
+      } catch (e) {
+        alert('Error')
+      } finally {
+        this.isPostLoading = true;
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app__btns{
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+}
+
+.app{
+  padding: 20px
 }
 </style>
---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <select
+      v-model="modelValue"
+      @change="cnangeOption"
+  >
+    <option disabled value="">Выберите из списка</option>
+    <option
+        v-for="option in options"
+        :key="options.value"
+        :value="options.value"
+    >
+      {{option.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "my-select",
+  props:{
+    modelValue: {
+      type: String
+    },
+    options:{
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    cnangeOption(event) {
+      console.log(event)
+      console.log(event.target.value)
+      this.$emit('update:modelValue', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
